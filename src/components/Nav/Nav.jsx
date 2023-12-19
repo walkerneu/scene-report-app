7282af7112d5789fd4462af04e0981fb661e597a
Walker Neudorff <walkerneudorff@gmail.com>
2023-12-19 13:50:21 -0600

    Edited Nav header to use MUI, incorporate drop down menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,45 +1,98 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
 
 function Nav() {
   const user = useSelector((store) => store.user);
+  const history = useHistory();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (num) => {
+    setAnchorEl(null);
+    if (num === 1) {
+      history.push("/");
+    } else if (num === 2) {
+      history.push("/event/add");
+    }
+  };
 
   return (
     <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
+          <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="black"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            {/* If no user is logged in, show these links */}
+             {!user.id && (
+               // If there's no user, show login/registration links
+              <Link className="navLink" to="/login">
+                 <MenuItem>Login / Register</MenuItem>
+              </Link>
+            )}
+            {/* If a user is logged in, show these links */}
+            {user.id && (
+              <div>
+              <MenuItem onClick={() => handleClose(1)}>Home</MenuItem>
+              <MenuItem onClick={() => handleClose(2)}>Add an Event!</MenuItem>
+              <Link className="navLink" to="/user">
+                Home
+              </Link>
+
+              <Link className="navLink" to="/info">
+                Info Page
+              </Link>
+
+              <LogOutButton className="navLink" />
+              </div>
+            )}
+            <Link className="navLink" to="/about">
+              About
+            </Link>
+          </Menu>
+          <Link to="/home">
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+            Scene Report! Powered by Slurp™ Industries!
+          </Typography>
           </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
     </div>
   );
 }
--- src/redux/sagas/events.saga.js
+++ src/redux/sagas/events.saga.js
@@ -1,20 +1,6 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
-function* getCreatedEvent(){
-    try {
-        const response = yield axios({
-            method: "GET",
-            url: `/api/event/created`
-        })
-        yield put ({
-            type: "SET_CURRENT_EVENT",
-            payload: response.data
-        })
-    } catch (error) {
-        console.log('Aw man, error in Saga get created event', error)
-    }
-}
 function* getCurrentEvent(action){
     try {
         const response = yield axios({
@@ -55,7 +41,6 @@ function* getUsersEvents(action){
 }
 
 function* eventsSaga() {
-    yield takeLatest('SAGA/GET_CREATED_EVENT', getCreatedEvent);
     yield takeLatest('SAGA/GET_CURRENT_EVENT', getCurrentEvent);
     yield takeLatest('SAGA/ATTEND_EVENT', attendEvent);
     yield takeLatest('SAGA/GET_USER_EVENTS', getUsersEvents);
